4f386d8c882922b2db9323a6a0f8d9ff93d07f65
JackMountain77 <salabang@gmail.com>
2025-08-26 23:08:19 +0900

    2508262308

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,11 +2,11 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().url(),
-  DIRECT_URL: z.string().url(),
+  DATABASE_URL: z.string(),
+  DIRECT_URL: z.string(),
 
   NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
+  NEXTAUTH_URL: z.string(),
 
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
