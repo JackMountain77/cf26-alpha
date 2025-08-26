896f1c30d99e6567d57f835b40e7ca6ddf3bdf6f
JackMountain77 <salabang@gmail.com>
2025-08-26 23:02:22 +0900

    2508262302

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,1 +1,15 @@
-// enviroment variables schema validation
+// src/lib/env.ts
+import { z } from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  DIRECT_URL: z.string().url(),
+
+  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_URL: z.string().url(),
+
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
