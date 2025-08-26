7acc021fb89bb1eacf37e08c99d18476aa343438
JackMountain77 <salabang@gmail.com>
2025-08-26 13:12:52 +0900

    2508261312

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -24,7 +24,7 @@ export async function GET() {
     }
 
     const user = await prisma.user.findUnique({
-      where: { email: session.user.email },
+      where: { email: session.user.email! },
       select: {
         id: true,
         email: true,
